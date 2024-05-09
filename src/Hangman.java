0dc1c5c812d2a1f76676e071f79dfcc9f8fdc49d
LukaPittman <luka.pittman@smus.ca>
2024-05-09 14:55:46 -0700

    List Created

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,8 +1,65 @@
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 public class Hangman {
-	
+	public static List<String> getWords() {
+        List<String> words = new ArrayList<>();
+        words.add("computer");
+        words.add("programming");
+        words.add("hangman");
+        words.add("developer");
+        words.add("algorithm");
+        words.add("software");
+        words.add("application");
+        words.add("interface");
+        words.add("database");
+        words.add("variable");
+        words.add("network");
+        words.add("keyboard");
+        words.add("monitor");
+        words.add("mouse");
+        words.add("server");
+        words.add("client");
+        words.add("debugging");
+        words.add("iteration");
+        words.add("repository");
+        words.add("version");
+        words.add("index");
+        words.add("compiler");
+        words.add("framework");
+        words.add("language");
+        words.add("script");
+        words.add("library");
+        words.add("backend");
+        words.add("frontend");
+        words.add("operator");
+        words.add("operand");
+        words.add("syntax");
+        words.add("statement");
+        words.add("expression");
+        words.add("inheritance");
+        words.add("polymorphism");
+        words.add("encapsulation");
+        words.add("abstraction");
+        words.add("constructor");
+        words.add("destructor");
+        words.add("parameter");
+        words.add("argument");
+        words.add("override");
+        words.add("overload");
+        words.add("class");
+        words.add("object");
+        words.add("method");
+        words.add("variable");
+        words.add("java");
+        words.add("python");
+        words.add("ruby");
+        words.add("html");
+        words.add("css");
+        return words;
+    }
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
 
 	}
 
