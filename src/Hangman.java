1b0a12f9d83c54794d8aad21cb4a3f649be841c6
LukaPittman <luka.pittman@smus.ca>
2024-05-09 15:11:00 -0700

    Change2

--- src/Hangman.java
+++ src/Hangman.java
@@ -62,9 +62,8 @@ public class Hangman {
     }
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		randomWord = [""];
-		index = 
-		Word = randomWord[index];
+		
+		
 	}
 
 }
