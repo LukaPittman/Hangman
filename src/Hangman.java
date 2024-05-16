bf665d73f66fbc365df4cdd9503bd84092c7524a
LukaPittman <luka.pittman@smus.ca>
2024-05-16 14:37:37 -0700

    hello2

--- src/Hangman.java
+++ src/Hangman.java
@@ -228,17 +228,9 @@ public Hangman() {
 				}
 			}
 			
-<<<<<<< Updated upstream
 
-				
-			if(wrongGuess==1) {
-				int centerX = 600;
-			    int centerY = 400;
-			    int radius = 50;
-			    // Draw the circle
-				globalGraphics.fillOval(centerX - radius, centerY - radius, 2 * radius, 2 * radius);
 
-=======
+		
 				if(wrongGuess==1) {
 					int centerX = 600;
 			        int centerY = 400;
@@ -246,7 +238,7 @@ public Hangman() {
 					// Draw the circle
 					g.fillOval(centerX - radius, centerY - radius, 2 * radius, 2 * radius);
 					
->>>>>>> Stashed changes
+
 				}
 			for(int g=0; g<=characters.size()-1; g++) { //goes through letters in chosenword
 				if (characters.get(g) == letter) { //guess is some or 1 letter in chosenword
